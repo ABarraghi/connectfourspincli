1a1487fc9b4c441f2195928e4fed41be4a4df846
abarraghi <abarraghi@gmail.com>
2024-09-13 12:53:03 -0400

    flipCol function first version implemented

--- ConnectFourSpin/Program.cs
+++ ConnectFourSpin/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Flips a column for a Connect Four Spin game
+//column of size 8
+//* indicates empty
+//X indicates player 1
+//O indicates player 2
+//ex. * * * X O * * * -> * * * O X * * *
+static char[] flipCol(char[] colToFlip)
+{
+    Array.Reverse(colToFlip);
+    return colToFlip;
+}
+
+char[] col = { '*', 'X', 'O' };
+Console.WriteLine(flipCol(col)); // Expecting 'O', 'X', '*'
 
